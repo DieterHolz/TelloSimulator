8ada05e2707525fbde03855f4b12176c26588216
dieter <dieter.holz@fhnw.ch>
2020-02-19 15:12:47 +0100

    initial commit

---
+++ src/main/java/tellosimulator/SomeClass.java
@@ -0,0 +1,14 @@
+package tellosimulator;
+
+/**
+ * Beispiel wie Code in adoc integriert werden kann
+ */
+public class SomeClass {
+
+    // tag::someMethod[]
+    public int someMethod(){
+        return 42;
+    }
+    // end::someMethod[]
+
+}
---
+++ src/test/java/tellosimulator/SomeClassTest.java
@@ -0,0 +1,14 @@
+package tellosimulator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class SomeClassTest {
+    @Test
+    void testProjectSetup(){
+        assertTrue(true);
+    }
+
+}
