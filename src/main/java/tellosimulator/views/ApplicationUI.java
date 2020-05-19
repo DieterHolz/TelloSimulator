d0d0533d12d7dfb6cc722a609c5aab987fd3d8a4
severin0038 <severin.peyer@students.fhnw.ch>
2020-05-19 11:00:46 +0200

    refactoring of initialize3dRoom

--- src/main/java/tellosimulator/views/ApplicationUI.java
+++ src/main/java/tellosimulator/views/ApplicationUI.java
@@ -1,10 +1,10 @@
 package tellosimulator.views;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
-import javafx.scene.shape.Sphere;
 import tellosimulator.TelloSimulator;
 import tellosimulator.drone.TelloDrone;
 
@@ -37,51 +37,33 @@ public class ApplicationUI extends StackPane {
 
     private void initialize3dRoom(double width, double height, double depth, double wallDepth) {
 
-        PhongMaterial red = new PhongMaterial();
-        red.setDiffuseColor(Color.RED);
-        PhongMaterial blue = new PhongMaterial();
-        blue.setDiffuseColor(Color.BLUE);
-        PhongMaterial green = new PhongMaterial();
-        green.setDiffuseColor(Color.GREEN);
-        PhongMaterial yellow = new PhongMaterial();
-        yellow.setDiffuseColor(Color.YELLOW);
-        PhongMaterial black = new PhongMaterial();
-        black.setDiffuseColor(Color.BLACK);
-
-        Box wallBack = new Box(width, height, wallDepth);
-        wallBack.setMaterial(red);
-        wallBack.translateXProperty().set(width/2);
-        wallBack.translateYProperty().set(-height/2);
-        wallBack.translateZProperty().set(depth);
-
-        Box wallRight = new Box(wallDepth, height, depth);
-        wallRight.setMaterial(blue);
-        wallRight.translateXProperty().set(width);
-        wallRight.translateYProperty().set(-height/2);
-        wallRight.translateZProperty().set(depth/2);
-
-        Box wallLeft = new Box(wallDepth, height, depth);
-        wallLeft.setMaterial(green);
-        wallLeft.translateYProperty().set(-height/2);
-        wallLeft.translateZProperty().set(depth/2);
-
-        Box ceiling = new Box(width, wallDepth, depth);
-        ceiling.setMaterial(yellow);
-        ceiling.translateXProperty().set(width/2);
-        ceiling.translateYProperty().set(-height);
-        ceiling.translateZProperty().set(depth/2);
-
-        Box floor = new Box(width, wallDepth, depth);
-        floor.setMaterial(black);
-        floor.translateXProperty().set(width/2);
-        floor.translateZProperty().set(depth/2);
-
         Group room3D = new Group();
-        room3D.getChildren().addAll(wallBack, wallRight, wallLeft, ceiling, floor);
+        room3D.getChildren().addAll(
+                prepareBox(width, height, wallDepth, Color.RED, width/2, -height/2, depth), //wallBack
+                prepareBox(wallDepth, height, depth, Color.BLUE, width, -height/2, depth/2), //wallRight
+                prepareBox(wallDepth, height, depth, Color.GREEN, 0, -height/2, depth/2), //wallLeft
+                prepareBox(width, wallDepth, depth, Color.YELLOW, width/2, -height, depth/2), //ceiling
+                prepareBox(width, wallDepth, depth, Color.BLACK, width/2, 0, depth/2)); //floor
         room3D.translateXProperty().set(width/2);
         room3D.translateYProperty().set(-height/2);
 
         this.getChildren().add(room3D);
+    }
+
+    private Box prepareBox(double xDimension, double yDimension, double zDimension, Color color, double xTranslate, double yTranslate, double zTranslate) {
+
+        Box box = new Box(xDimension, yDimension, zDimension);
+
+        PhongMaterial material = new PhongMaterial();
+        material.setDiffuseColor(color);
+
+        box.setMaterial(material);
+
+        box.translateXProperty().set(xTranslate);
+        box.translateYProperty().set(yTranslate);
+        box.translateZProperty().set(zTranslate);
+
+        return box;
     }
 
 }
