3323c1ddcc02148e9fdd00d724d76fdc9cebcaa6
severin0038 <severin.peyer@students.fhnw.ch>
2020-08-10 15:43:30 +0200

    correct positioning of the walls, change wall colors, add lights

--- src/main/java/tellosimulator/views/Room3d.java
+++ src/main/java/tellosimulator/views/Room3d.java
@@ -1,22 +1,40 @@
 package tellosimulator.views;
 
+import javafx.scene.AmbientLight;
 import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.PointLight;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
+import javafx.scene.shape.Sphere;
+import javafx.scene.transform.Translate;
+
+import java.awt.*;
 
 public class Room3d {
 
     private Group room3D;
+    private double width;
+    private double height;
+    private double depth;
+    private double wallDepth;
 
     public Room3d(double width, double height, double depth, double wallDepth) {
+        this.width = width;
+        this.height = height;
+        this.depth = depth;
+        this.wallDepth = wallDepth;
+
         room3D = new Group();
         room3D.getChildren().addAll(
-                prepareBox(width, height, wallDepth, Color.RED, width/2, -height/2, depth), //wallBack
-                prepareBox(wallDepth, height, depth, Color.BLUE, width, -height/2, depth/2), //wallRight
-                prepareBox(wallDepth, height, depth, Color.GREEN, 0, -height/2, depth/2), //wallLeft
-                prepareBox(width, wallDepth, depth, Color.YELLOW, width/2, -height, depth/2), //ceiling
-                prepareBox(width, wallDepth, depth, Color.BLACK, width/2, 0, depth/2)); //floor
+                prepareBox(width, height, wallDepth, Color.grayRgb(220), width/2, -height/2, depth+wallDepth/2), //wallBack
+                prepareBox(wallDepth, height, depth, Color.grayRgb(220), width+wallDepth/2, -height/2, depth/2), //wallRight
+                prepareBox(wallDepth, height, depth, Color.grayRgb(220), 0-wallDepth/2, -height/2, depth/2), //wallLeft
+                prepareBox(width, wallDepth, depth, Color.grayRgb(220), width/2, -height-wallDepth/2, depth/2), //ceiling
+                prepareBox(width, wallDepth, depth, Color.grayRgb(220), width/2, 0+wallDepth/2, depth/2) //floor
+        );
+        room3D.getChildren().addAll(prepareLightSources());
     }
 
     public Group getRoom3d() {
@@ -37,5 +55,16 @@ public class Room3d {
         box.translateZProperty().set(zTranslate);
 
         return box;
+    }
+
+    private Node[] prepareLightSources() {
+        PointLight pointLight = new PointLight();
+        pointLight.setColor(Color.rgb(252,252,255));
+        pointLight.getTransforms().add(new Translate(width/2,-height+wallDepth,depth/2));
+
+        AmbientLight ambientLight = new AmbientLight();
+        ambientLight.setColor(Color.rgb(100,100,100));
+
+        return new Node[]{pointLight, ambientLight};
     }
 }
--- src/main/java/tellosimulator/views/Simulator3DScene.java
+++ src/main/java/tellosimulator/views/Simulator3DScene.java
@@ -14,10 +14,8 @@ public class Simulator3DScene extends SubScene {
     public static final double WALL_DEPTH = 0.01;
 
     public Simulator3DScene(Stage stage, Parent sceneGraph) {
-        super(sceneGraph, Math.max(500, ROOM_WIDTH), Math.max(500, ROOM_HEIGHT), true, SceneAntialiasing.BALANCED);
+        super(sceneGraph, ROOM_WIDTH*2, ROOM_HEIGHT*2, true, SceneAntialiasing.BALANCED);
         setupCamera();
-
-        setFill(Color.BLUE);
 
         initMouseControl(stage, sceneGraph);
     }
@@ -32,7 +30,7 @@ public class Simulator3DScene extends SubScene {
 
     private void setupCamera() {
         Camera camera = new PerspectiveCamera();
-        camera.translateXProperty().set(-ROOM_WIDTH/2.5);
+        camera.translateXProperty().set(-ROOM_WIDTH/2);
         camera.translateYProperty().set(-ROOM_HEIGHT*1.5);
         camera.translateZProperty().set(0);
         setCamera(camera);
