4cd0e062b6b00897ad3b0fcb570e8482f5566466
danielobrist <daniel.obrist@students.fhnw.ch>
2020-11-26 14:25:48 +0100

    fix rc test

--- src/test/java/tellosimulator/view/DroneControllerTest.java
+++ src/test/java/tellosimulator/view/DroneControllerTest.java
@@ -8,7 +8,7 @@ import tellosimulator.command.TelloSetCommand;
 import tellosimulator.controller.DroneController;
 import tellosimulator.model.DroneModel;
 import tellosimulator.network.CommandConnection;
-import tellosimulator.network.TelloSDKValues;
+import tellosimulator.common.TelloSDKValues;
 import tellosimulator.view.drone.DroneView;
 
 import java.io.IOException;
@@ -32,27 +32,27 @@ class DroneControllerTest {
         droneController = new DroneController(droneModel);
         commandConnection = new CommandConnection(droneController);
         commandHandler = new CommandHandler(droneController);
-        commandPackage = new CommandPackage(null, InetAddress.getByName(TelloSDKValues.getOperatorIpAddress()), TelloSDKValues.SIM_COMMAND_PORT);
+        commandPackage = new CommandPackage(null, InetAddress.getByName("127.0.0.1"), TelloSDKValues.SIM_COMMAND_PORT);
     }
 
-    private void computeResponse(String command) throws IOException {
+    private void computeResponse(String command) {
         commandPackage.setCommand(command);
         commandHandler.handle(commandPackage);
     }
 
     @Test
-    void testRcHandling() throws IOException {
-        assertEquals(0, droneController.getDroneModel().getForwardBackwardDiff());
+    void testRcHandling() {
         assertEquals(0, droneController.getDroneModel().getLeftRightDiff());
+        assertEquals(0, droneController.getDroneModel().getForwardBackwardDiff());
         assertEquals(0, droneController.getDroneModel().getUpDownDiff());
         assertEquals(0, droneController.getDroneModel().getYawDiff());
 
         computeResponse(TelloSetCommand.RC + " 0 10 20 30");
 
-        assertEquals(0, droneController.getDroneModel().getForwardBackwardDiff());
-        assertEquals(10, droneController.getDroneModel().getLeftRightDiff());
+        assertEquals(0, droneController.getDroneModel().getLeftRightDiff());
+        assertEquals(10, droneController.getDroneModel().getForwardBackwardDiff());
         assertEquals(20, droneController.getDroneModel().getUpDownDiff());
-        assertEquals(30, droneController.getDroneModel().getYawDiff());
+        assertEquals(-30, droneController.getDroneModel().getYawDiff());
     }
 
 }
